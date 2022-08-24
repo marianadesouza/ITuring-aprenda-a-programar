053107820bb95755f03e42b163c841a198042f93
marianadesouza <marianaguedesareas@gmail.com>
2022-08-24 11:41:07 -0300

    adicionar valor as variaveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,6 @@
-let nome 
-let email 
-let profissao 
-let idade
-let vacationspot 
+let nome = "Mariana"
+let email = "marianaguedesareas@gmail.com"
+let profissao = "estudante"
+let idade = 17
+let vacationspot = "Italia"
+
